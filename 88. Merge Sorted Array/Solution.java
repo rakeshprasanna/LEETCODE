811849d219dfb054c41c7335c432db3d81e60e8d
Rakesh Prasanna R <rakeshprasanna72@gmail.com>
2022-07-24 01:46:33 +0530

    Create Solution.java

---
+++ 88. Merge Sorted Array/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int pointToEnd =m+n-1;
+        
+        while(m>0 && n>0){
+            if(nums1[m-1]>nums2[n-1]) {
+                nums1[pointToEnd]=nums1[m-1];
+                m--;
+            }
+            
+            else {
+                nums1[pointToEnd]=nums2[n-1];
+                n--;
+            }
+            pointToEnd--;
+        }
+        
+        while(n>0){
+            nums1[pointToEnd]=nums2[n -1];
+            n--;
+            pointToEnd--;
+        }
+    }
+}
